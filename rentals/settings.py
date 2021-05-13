e16ed507c714c19a7c27f70e639237bf3dd8074f
Tannar <tannar.james@gmail.com>
2021-05-13 09:57:27 -0500

    Update Property App

--- rentals/settings.py
+++ rentals/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'property.apps.PropertyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'rentals.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '#Raiden17',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- rentals/urls.py
+++ rentals/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.home)
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
